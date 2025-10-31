649b283013b5157458b0c744420783d364d382f4
Sharanya-katukuri <katukurisharanya@gmail.com>
2025-10-31 22:41:37 +0530

    functions

---
+++ day7/functions.js
@@ -0,0 +1,37 @@
+// function:it is a block of code ,used to perform a specific task
+// we should invoke a function
+
+// to create a function---by using function keyword
+/*  function function_name(){
+        // set og code
+}
+function_name()
+*/
+// uses/advantages
+// 1.reusability:we can reuse code,by calling the function name
+
+// named function: function  with the  function name
+function greet(){
+    console.log("hello world")
+}
+greet()
+
+function fun_name(){
+    var a=4
+    var b=3
+    var c=a+b
+    console.log(c)
+
+}
+fun_name()
+
+// parametrized function():the function by passing the parameter
+function add(a,b){
+    console.log(a+b)
+    console.log(a*b)
+}
+add(10,30)
+
+// parameters:the variable which is holding  the values
+// arguments:the value we are passing to the function 
+
