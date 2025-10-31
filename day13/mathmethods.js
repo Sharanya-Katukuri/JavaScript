9a50b259827c549905c0ab3c4ccc8748620843db
Sharanya-katukuri <katukurisharanya@gmail.com>
2025-10-31 23:07:46 +0530

    math methods

--- day13/mathmethods.js
+++ day13/mathmethods.js
@@ -1,3 +1,50 @@
-var num=2356.65
-console.log(num.toPrecision(5))
-console.log(num)
+// MATHMETHODS
+// Math.abs():retruns positive value
+var a=-12
+console.log(Math.abs(a))
+console.log(Math.abs(-13452))
+
+// Math.round():round a number to nearest integer
+console.log(Math.round(15.345))
+console.log(Math.round(15.564))
+
+// Math.ceil():rounds a number up to the nearest integer
+
+console.log(Math.ceil(16.3458))
+console.log(Math.ceil(19.798))
+
+//Math.foolr():round a number down to the nearest integer
+
+console.log(Math.floor(16.3458))
+console.log(Math.floor(19.798))
+
+// Math.trunc():removes the frational part,returning the integer part
+
+console.log(Math.trunc(4.5687))
+console.log(Math.trunc(2454.9687))
+
+// Math.min():returns smallest number amoung arguments
+
+console.log(Math.min(2,3,67,89,24,1,76))
+
+//Math.max():returns largest number amoung arguments
+
+console.log(Math.max(2,3,67,89,24,1,76))
+
+//Math.pow():calculate exponential(base,exponent)
+
+console.log(Math.pow(4,2))
+console.log(Math.pow(2,8))
+
+//Math.sqrt():returns square root
+
+console.log(Math.sqrt(81))
+console.log(Math.sqrt(121))
+console.log(Math.sqrt(5))
+
+// Math.random():generate ranndom number between 0 to 1
+
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
